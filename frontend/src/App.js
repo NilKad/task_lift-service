d3e53f46d0bc9293a8808dd9598c24aedbcd31d3
NilKad <kadulin@gmail.com>
2023-06-05 22:17:28 +0300

    add react

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './shared/ShareLayout';
+// import { LiftService } from './pages/LiftService';
+
+function App() {
+  // return <SharedLayout>APP Shared Layout</SharedLayout>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
+// <Routes>
+//   <Route path="/" element={<SharedLayout />}>
+//   <Route index element={<LiftService />} />
+//   </Route>
+//  </Routes>
---
+++ frontend/src/shared/ShareLayout.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom"
+// import { Header } from "../components/Header/Header"
+
+export const SharedLayout = () => {
+  return (<><Outlet /></>)
+
+
+}
