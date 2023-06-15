4d5d8fd37d0e74f18a1724f20630403b6d1e5885
NilKad <kadulin@gmail.com>
2023-06-16 02:21:26 +0300

    render.com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import { SharedLayout } from './shared/ShareLayout';
 import { LiftService } from './pages/LiftService';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://localhost:5001/api';
-axios.defaults.baseURL = 'https://lift-service.onrender.com/api';
+axios.defaults.baseURL = 'http://localhost:5001/api';
+// axios.defaults.baseURL = 'https://lift-service.onrender.com/api';
 
 function App() {
   // return <SharedLayout>APP Shared Layout</SharedLayout>;
