3994834a0cc181fd4d4ff53083b0b0f5109f6c45
NilKad <kadulin@gmail.com>
2023-06-25 01:34:27 +0300

    add basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { GlobalStyle } from './components/BasesStyles/GlobalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/task_lift-service">
     <GlobalStyle />
     <App />
   </BrowserRouter>
