7a556fd63584e3ced36aa4e7b4368489bf05e984
NilKad <kadulin@gmail.com>
2023-06-25 01:22:19 +0300

    workflow

--- frontend/src/pages/LiftService.jsx
+++ frontend/src/pages/LiftService.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from '../components/Container/Container';
 import { FloorCallPanels } from '../components/FloorCallPanels/FloorCallPanels';
 import { LiftInternalPanel } from '../components/LiftInternalPanel/LiftInternalPanel';
